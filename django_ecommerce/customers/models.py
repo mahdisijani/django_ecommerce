655cebd6cf47736deb9967516b8cfb7a13c8d8f7
mahdisj <mahdi.cj@gmail.com>
2021-09-09 12:34:56 +0430

    update

--- django_ecommerce/customers/models.py
+++ django_ecommerce/customers/models.py
@@ -1,8 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import RESTRICT
 # Create your models here.
 
+
+class User(AbstractUser):
+    # Boss = 1
+    # Professor = 2
+    # Staff = 3
+    class_role_choices = (
+        (1, 'Admin'),
+        (2, 'Staff'),
+        (3, 'Customer'),
+    )
+    role = models.PositiveSmallIntegerField(choices=class_role_choices,
+                                            default=3)
+
+
 class Customer(models.Model):
+    user = models.OneToOneField(
+        User, related_name='customer', on_delete=RESTRICT)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.TextField()
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- django_ecommerce/orders/models.py
+++ django_ecommerce/orders/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from customers.models import Customer
 from products.models import Product
+
 # Create your models here.
 
-User = get_user_model()
+
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+    customer = models.ForeignKey(Customer, on_delete=models.RESTRICT,related_name='order')
     date = models.DateTimeField(auto_now_add=True)
     status_code = (('1', 'cancel'),
                    ('2', 'success'),
@@ -16,12 +17,32 @@ class Order(models.Model):
                    ('6', 'active'))
     status = models.CharField(choices=status_code, max_length=20)
 
+    def __str__(self):
+        return self.id
+
+
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name='item')
     item = models.ForeignKey(Product, on_delete=models.RESTRICT)
     quantity = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f"{self.order}-{self.item}"
 
 
 class Payment(models.Model):
     order = models.OneToOneField(Order, on_delete=models.RESTRICT)
     payment_info = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.id
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE,related_name='shippingaddress')
+    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    city=models.CharField(max_length=50)
+    address=models.TextField()
+    postalcode=models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return self.customer
