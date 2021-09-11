30a27b49e65b066822c675921482f9eb254e59a9
mahdisj <58192360+mahdisj@users.noreply.github.com>
2021-09-11 17:30:55 +0430

    Update urls.py

--- django_ecommerce/products/urls.py
+++ django_ecommerce/products/urls.py
@@ -7,13 +7,6 @@ urlpatterns = [
     # first argument is the url prefix,
     path('', index, name='index'),
     path('product/<int:product_id>/', product, name='product'),
-    # path('class_list', ClassView.as_view(), name='class_list'),
-    # path('class/<int:class_id>/', class_details, name='class_details'),
-    # path('student_list', StudentView.as_view(), name='student_list'),
-    # path('create_student/', create_student, name='create_student'),
-    
-    # path('take_courses/<int:std_id>/', take_courses, name='take_courses'),
-    # path('login/', user_login, name='login'),
-    # path('user_register/', user_register, name='user_register'),
+
     
 ]
